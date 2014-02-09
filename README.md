7f7490c19fd012b610875d845edff5ace02946ad
Clare Curtis <cfcurti2@illinois.edu>
2014-02-09 13:07:47 -0600

    basic stuff added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ emailviz
 ========
 
 CS 467 email visualization SP2014
+
+To run setup virtualenv and pip install Flask then run with command "python routes.py"
---
+++ routes.py
@@ -0,0 +1,11 @@
+from run import app
+from flask import Flask, render_template, request, url_for, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
