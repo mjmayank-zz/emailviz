752dc1fbde628ef0099bc5498dff7a8e1a8bb33c
Mayank Jain <mjmayank@gmail.com>
2014-02-11 23:04:02 -0600

    initial shitty version

--- routes.py
+++ routes.py
@@ -9,7 +9,8 @@ def index():
 @app.route('/viz')
 def viz():
 	emails = get_emails()
-	return render_template('viz.html', emails=emails)
+	return render_template('viz.html', emails=emails, test={2013: {'February': {'code': 5, 'scratch': 1, 'ascript': 1, 'resistance': 1, 'llu': 2, 'month': 1, 'screen': 1, 'follow': 1, 'displaying': 1, 'calculate': 1, 'e-mail': 3}, 
+																	'March': {'code': 5, 'scratch': 1, 'ascript': 1, 'resistance': 1, 'llu': 2, 'month': 1, 'screen': 1, 'follow': 1, 'displaying': 1, 'calculate': 1, 'e-mail': 3}}})
 
 if __name__ == '__main__':
 	app.debug = True
