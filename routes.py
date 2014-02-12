753d371a20928009b74cc6589f34be53ee5755a8
Clare Curtis <cfcurti2@illinois.edu>
2014-02-12 14:01:14 -0600

    Use python routes.py json to run from json file

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from downloademails import get_emails
+import json
+import sys
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +10,10 @@ def index():
 
 @app.route('/viz')
 def viz():
-	emails = get_emails()
+	if len(sys.argv) > 1 and str(sys.argv[1]) == 'json':
+		emails = json.load(open("emails.json", "r"))
+	else:
+		emails = get_emails()
 	return render_template('viz.html', emails=emails, test={2013: {'February': {'code': 5, 'scratch': 1, 'ascript': 1, 'resistance': 1, 'llu': 2, 'month': 1, 'screen': 1, 'follow': 1, 'displaying': 1, 'calculate': 1, 'e-mail': 3}, 
 																	'March': {'code': 5, 'scratch': 1, 'ascript': 1, 'resistance': 1, 'llu': 2, 'month': 1, 'screen': 1, 'follow': 1, 'displaying': 1, 'calculate': 1, 'e-mail': 3}}})
 
